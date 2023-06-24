aae099a7edb4594cf2204e389697885d1574ab66
Gek Siong Low <geksiong@yahoo.com>
2023-06-24 14:10:26 +0800

    refactor: move Posts into content folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import { defaultLayoutRemarkPlugin } from './src/utils/defaultlayout-remark-plugin.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.my-site.dev',
   markdown: {
-    remarkPlugins: [ defaultLayoutRemarkPlugin ]
+    remarkPlugins: [ ]
   },
-  extendDefaultPlugins: true,
 });
